26322b8e7f453873f64994a2599671e4acda7e47
xuanphu1008 <145204443+xuanphu1008@users.noreply.github.com>
2026-01-15 02:46:57 +0700

    Add files via upload

---
+++ Presentation/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Presentation.Forms;
+using BusinessLogic.Services;
+
+namespace Presentation
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            var loginservice = new LoginService();
+            Application.Run(new frmLogin(loginservice));
+        }
+    }
+}
